2c7c904225fa9ab85fb8c8cba052c0f57ea35606
rajat modi <rajatmodi62@gmail.com>
2019-09-10 10:44:08 +0530

    updated device information

--- unet_pipeline/utils/prepare_png_stage1.sh
+++ unet_pipeline/utils/prepare_png_stage1.sh
@@ -1,1 +1,1 @@
-python prepare_png.py -img_size 1024 -train_path ../../input/train test_path ../../input/test -out_path ../../input/dataset1024 -rle_path ../../input/train-rle.csv -n_threads 8
+python prepare_png.py -img_size 1024 -train_path ../../input/dicom_train -test_path ../../input/dicom_test -out_path ../../input/dataset1024 -rle_path ../../input/train-rle.csv -n_threads 1
